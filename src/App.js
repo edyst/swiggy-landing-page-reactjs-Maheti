85da53938e64bf3f21f8fd46423e3aaea235cef1
Maheti Mehra <mahetimehra@gmail.com>
2021-09-22 00:24:22 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Herosection from "./Components/Herosection/hero";
+import Features from "./Components/Features/features";
+import Restaurants from "./Components/Restaurants/restaurants";
+import Footer from "./Components/Footer/footer";
+
+import "./App.css";
+import "./Components/FontAwesome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <Herosection />
+      <Features />
+      <Restaurants />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Herosection/Buttons/button.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "../hero.css";
+
+const button = (props) => {
+  return <button className={props.classes}>{props.name}</button>;
+};
+export default button;
